2e9fee8669cb556b3103898afc28e083199df792
Sergey Kichigin <kicha3007@gmail.com>
2017-02-20 11:50:08 +0500

    add items

--- markup/components/nav/data/data.js
+++ markup/components/nav/data/data.js
@@ -1,1 +1,12 @@
-var data = {nav: {}};
+data = {
+    menu: {
+        items: {
+            "/index.html": "Главная страница",
+            "/news.html": "Новости",
+            "/reviews.html": "Отзывы"
+        },
+        content: {
+            "className": "nav"
+        }
+    }
+};
