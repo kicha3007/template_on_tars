27b9527f88ff25e8ddbc0225f5221933a1c037b1
Sergey Kichigin <kicha3007@gmail.com>
2017-03-13 10:11:57 +0500

    dropdown menu in process

---
+++ markup/components/nav/data/data.js
@@ -0,0 +1,87 @@
+var data = {
+    nav: {
+        settings: {
+            base: [
+                {
+
+                }
+            ]
+        },
+        nav1: {
+            items: [
+                {
+                    href: "index.html",
+                    text: "Главная страница"
+                },
+                {
+                    href: "news.html",
+                    text: "Новости"
+                },
+                {
+                    href: "reviews.html",
+                    text: "Отзывы"
+                },
+                {
+                    href: "reviews.html",
+                    text: "Страница"
+                },
+                {
+                    href: "reviews.html",
+                    text: "Страница"
+                },
+                {
+                    href: "reviews.html",
+                    text: "Страница"
+                },
+                {
+                    href: "reviews.html",
+                    text: "Страница"
+                },
+                {
+                    href: "reviews.html",
+                    text: "Страница"
+                },
+                {
+                    title: "Заголовок второго уровня 5",
+                    link: "#",
+                    col: true,
+                    dropdown: true,
+                    submenu:[
+                        {
+                            title: "Заголовок третьего уровня 1",
+                            link: "#"
+                        },
+                        {
+                            title: "Заголовок третьего уровня 1",
+                            link: "#"
+                        },
+                        {
+                            title: "Заголовок третьего уровня 1",
+                            link: "#"
+                        },
+                        {
+                            title: "Заголовок третьего уровня 1",
+                            link: "#"
+                        }
+                    ]
+                }
+            ]
+        },
+        nav2: {
+            items: [
+                {
+                    href: "index.html",
+                    text: "Вотfff"
+                },
+                {
+                    href: "news.html",
+                    text: "Так"
+                },
+                {
+                    href: "reviews.html",
+                    text: "Вот"
+                }
+            ]
+        }
+    }
+};
